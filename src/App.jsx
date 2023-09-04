f171bad74cfbfdbc53751c1a4fc19bcd69baea48
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-09-04 17:42:31 -0300

    chamando componentes no app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
-
+import Menu from './components/Menu'
+import Rodape from './components/Rodape'
 
 function App() {
   return
   <>
+     <Menu/>
     <Outlet/>
+    <Rodape/>
   </>
 }
 export default App
