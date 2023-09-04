66aaf0d2db493f8296c4cbf2e0f1f8ef0ef579db
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-09-04 17:56:16 -0300

    arquivo JS com um array de produtos,

---
+++ src/components/listaProdutos.js
@@ -0,0 +1,17 @@
+export const listaProdutos = [
+    {
+       id:1,
+       nome:'teclado',
+       preco:150
+    },
+    {
+        id:2,
+        nome:'Mouse',
+        preco:120
+    },
+    {
+        id:3,
+        nome:'Monitor',
+        preco:950
+    },
+]
