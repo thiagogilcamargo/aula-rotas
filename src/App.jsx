ae27079209ef9f8e4f60ca99724142331aba01eb
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-09-04 18:19:04 -0300

    correção

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Menu from './components/Menu'
 import Rodape from './components/Rodape'
 
 function App() {
-  return
-  <>
-    <Menu/>
-    <Outlet/>
-    <Rodape/>
-  </>
+  return (
+    <>
+      <Menu />
+      <Outlet />
+      <Rodape />
+    </>
+  );
 }
-export default App
+
+export default App;
