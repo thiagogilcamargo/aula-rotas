151dab7d5680311fc9006fb99d6465de192fec3d
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-09-04 18:31:27 -0300

    correção de rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,35 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Home from './routes/Home/index.jsx'
-import Produtos from './routes/Produtos/index.jsx'
-import EditarProdutos from './routes/EditarProdutos/index.jsx'
-import App from "./App.jsx"
-
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App'; // Importe o componente App corretamente
+import Home from './routes/Home/index.jsx';
+import Produtos from './routes/Produtos/index.jsx';
+import EditarProdutos from './routes/EditarProdutos/index.jsx';
 
 const router = createBrowserRouter([
-{
-  path: "/",
-  element: <App />,
-  errorElement: <Error />,
-  children:[
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/produtos",
-      element: <Produtos />,
-    },
-    {
-      path: "/produto/editar/:id", // Use :id para criar um parâmetro dinâmico
-      element: <EditarProdutos />,
-    },
-  ],
-},
+  {
+    path: '/',
+    element: <App />, // Certifique-se de que o componente App esteja definido e importado corretamente
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/produtos',
+        element: <Produtos />,
+      },
+      {
+        path: '/produto/editar/:id',
+        element: <EditarProdutos />,
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<React.StrictMode>
-  <RouterProvider router={router} />
-</React.StrictMode>,
-
-) 
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
