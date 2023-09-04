6f0973d9241ddf610311ec8d8e47f727a07110ee
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-09-04 15:58:00 -0300

    renderização das páginas no ReactDOM

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,36 @@
-  import { createBrowserRouter, RouterProvider } from "react-router-dom"
-  import Home from './routes/Home/index.jsx'
-  import Produtos from './routes/Produtos/index.jsx'
-  import Error from './routes/Error/Index.jsx'
-  import EditarProdutos from './routes/EditarProdutos/index.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from './routes/Home/index.jsx'
+import Produtos from './routes/Produtos/index.jsx'
+import Error from './routes/Error/Index.jsx'
+import EditarProdutos from './routes/EditarProdutos/index.jsx'
+
 
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <Error />,
-    children:[
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/produtos",
-        element: <Produtos />,
-      },
-      {
-        path: "/produto/editar/:id", // Use :id para criar um parâmetro dinâmico
-        element: <EditarProdutos />,
-      },
-    ],
-  },
+{
+  path: "/",
+  element: <App />,
+  errorElement: <Error />,
+  children:[
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/produtos",
+      element: <Produtos />,
+    },
+    {
+      path: "/produto/editar/:id", // Use :id para criar um parâmetro dinâmico
+      element: <EditarProdutos />,
+    },
+  ],
+},
 ]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+<React.StrictMode>
+  <RouterProvider router={router} />
+</React.StrictMode>,
+
+) 
