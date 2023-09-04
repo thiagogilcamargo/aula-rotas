43f13feb728375a62a10aad3dad68a467e9b19fb
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-09-04 15:37:06 -0300

    importando os elementos do router e as paginas

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
-  import { createBrowserRouter, RouterProvider } from "react-router-dom";
+  import { createBrowserRouter, RouterProvider } from "react-router-dom"
   import Home from './routes/Home/index.jsx'
-  import Produtos from "./routes/Produtos/index.jsx";
+  import Produtos from './routes/Produtos/index.jsx'
+  import Error from './routes/Error/Index.jsx'
+  import EditarProdutos from './routes/EditarProdutos/index.jsx'
