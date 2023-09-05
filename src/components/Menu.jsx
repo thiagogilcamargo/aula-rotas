dbab0cda106cc224beb17eee792c19f742beee11
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-09-05 00:14:30 -0300

    correçaõ de erros

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,12 @@
- import { link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Menu (){
-    return(
-        <nav className="menu">
-            <link to='/'>Home</link>
-            <span> | </span>
-            <link to='/produtos'>Produtos</link>
-        </nav>
-    )
+export default function Menu() {
+  return (
+    <nav className="menu">
+      <Link to="/">Home</Link>
+      <span> | </span>
+      <Link to="/produtos">Produtos</Link>
+    </nav>
+  );
 }
