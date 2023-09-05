72efe735bc03efff4d1e1ea08bc23895c766cba8
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-09-04 22:42:17 -0300

    ajustando rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Home from './routes/Home/index.jsx'
 import Produtos from './routes/Produtos/index.jsx'
