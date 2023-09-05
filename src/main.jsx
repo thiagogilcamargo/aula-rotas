71777e231ef57af1462fa8caf9b2e609a41d5e90
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-09-04 23:22:43 -0300

    Solucinando erro

--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-i
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Home from './routes/Home/index.jsx'
 import Produtos from './routes/Produtos/index.jsx'
 import EditarProduto from './routes/EditarProduto/index.jsx'
-import Error from './routes/Error/index.jsx'
-
-
+import Error from './routes/Error/Index.jsx'
 
 const router = createBrowserRouter([
-{
-  path: "/",
-  element: <App />,
-  errorElement: <Error />,
-  children:[
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/produtos",
-      element: <Produtos />,
-    },
-    {
-      path: "/produto/editar/:id", 
-      element: <EditarProduto/>,
-    },
-  ],
-},
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/produtos",
+        element: <Produtos />,
+      },
+      {
+        path: "/produto/editar/:id", 
+        element: <EditarProduto/>,
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<React.StrictMode>
-  <RouterProvider router={router} />
-</React.StrictMode>,
-
-) 
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
