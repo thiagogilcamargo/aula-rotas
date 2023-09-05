7b1fd4a29129510dbf386fce7567d87818579203
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-09-04 23:06:29 -0300

    +correção derotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+i
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Home from './routes/Home/index.jsx'
 import Produtos from './routes/Produtos/index.jsx'
 import EditarProduto from './routes/EditarProduto/index.jsx'
+import Error from './routes/Error/index.jsx'
 
 
 
