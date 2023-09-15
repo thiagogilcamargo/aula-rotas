3050a5c7483138102f80395e917e9d8953702250
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-09-15 19:44:02 -0300

    Passando valores pela URI

--- src/routes/EditarProduto/index.jsx
+++ src/routes/EditarProduto/index.jsx
@@ -1,8 +1,25 @@
+import { useParams,useNavigate } from "react-router-dom"
+import {listaProdutos} from "../../components/listaProdutos"
+
 export default function EditarProduto() {
-    
-    return (
-      <main>
-          <h1>Editando o Produto </h1>
-      </main>
-    )
+
+  const lista = listaProdutos
+  const navegacao = useNavigate()
+  const {id} = useParams()
+
+  const proc = lista.filter(prod => prod.id == id)
+  const produto = proc[0]
+
+  const salvar = () =>{
+      alert('Produto:${produto.nome} editando com sucesso!')
+      return navegacao('/produto')
   }
+
+  return(
+     <main>
+         <h1> Editando o produto </h1>
+         <p>Editando os dados do produto: {produto.nome}</p>
+         <button onClick={salvar}>Salvar</button>
+     </main>
+  )
+}
