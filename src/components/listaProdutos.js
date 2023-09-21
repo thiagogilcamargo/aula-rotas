b80a9df3be01a55c9f4969a8323ff900d97ed415
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-09-21 13:00:27 -0300

    correção id

--- src/components/listaProdutos.js
+++ src/components/listaProdutos.js
@@ -1,13 +1,13 @@
-import { v4 as uuidv4 } from 'uuid';
+
 
 export const listaProdutos = [
-    { id: uuidv4(), nome: "Teclado", desc: "Teclado 102 Teclas - Mecânico.", preco: 200, img: "https://picsum.photos/100/100" },
-    { id: uuidv4(), nome: "Mouse", desc: "Mouse Gamer - 3200 DPI.", preco: 100, img: "https://picsum.photos/100/100" },
-    { id: uuidv4(), nome: "Monitor", desc: "Monitor 24' - 144Hz.", preco: 800, img: "https://picsum.photos/100/100" },
-    { id: uuidv4(), nome: "Headset", desc: "Headset Gamer - 7.1 Surround.", preco: 300, img: "https://picsum.photos/100/100" },
-    { id: uuidv4(), nome: "Mousepad", desc: "Mousepad Gamer - 80x30cm.", preco: 50, img: "https://picsum.photos/100/100" },
-    { id: uuidv4(), nome: "Cadeira", desc: "Cadeira Gamer - 180°.", preco: 1000, img: "https://picsum.photos/100/100" },
-    { id: uuidv4(), nome: "Gabinete", desc: "Gabinete Gamer - Vidro Temperado.", preco: 500, img: "https://picsum.photos/100/100" },
-    { id: uuidv4(), nome: "Placa de Vídeo", desc: "Placa de Vídeo - 8GB.", preco: 2000, img: "https://picsum.photos/100/100" }
+    { id: "82e73477-260e-49c4-9c00-5c7ea0705f89", nome: "Teclado", desc: "Teclado 102 Teclas - Mecânico.", preco: 200, img: "https://picsum.photos/100/100" },
+    { id: "6df8a9f0-c1be-4578-96cd-f4ff56865662", nome: "Mouse", desc: "Mouse Gamer - 3200 DPI.", preco: 100, img: "https://picsum.photos/100/100" },
+    { id: "6d0dd888-2394-4c1a-9826-9fbfad0a3d50", nome: "Monitor", desc: "Monitor 24' - 144Hz.", preco: 800, img: "https://picsum.photos/100/100" },
+    { id: "e4cf7cbb-8974-46ad-93ae-311e0b362df9", nome: "Headset", desc: "Headset Gamer - 7.1 Surround.", preco: 300, img: "https://picsum.photos/100/100" },
+    { id: "eb4c0cee-45eb-4a6b-ae2c-939d2c4ef2d5", nome: "Mousepad", desc: "Mousepad Gamer - 80x30cm.", preco: 50, img: "https://picsum.photos/100/100" },
+    { id: "37f36b53-ecc2-4be6-a987-816f31cb815d", nome: "Cadeira", desc: "Cadeira Gamer - 180°.", preco: 1000, img: "https://picsum.photos/100/100" },
+    { id: "7b7bb79f-92ee-42e3-8821-9ddf9619c800", nome: "Gabinete", desc: "Gabinete Gamer - Vidro Temperado.", preco: 500, img: "https://picsum.photos/100/100" },
+    { id: "67e657b1-9e8d-4e7a-aff3-b91744ccd72d", nome: "Placa de Vídeo", desc: "Placa de Vídeo - 8GB.", preco: 2000, img: "https://picsum.photos/100/100" }
   ];
   
